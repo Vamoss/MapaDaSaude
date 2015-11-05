83ef81ba721c62a4bd05855f6c985e2e412f3048
Carlos de Oliveira Junior <vamoss@vamoss.com.br>
2015-11-05 02:47:59 -0200

    Start the UI with Bootstrap (related #2 #3 #4)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
-var TYPES = {
-	SEM_MEDICO:0,
-	DEMORA_ATENDIMENTO:1,
-	DEMORA_AMBULANCIA:2,
-	HOSPITAL_SEM_ESTRUTURA:3,
-	FALTA_MEDICACAO:4,
-	NEGLIGENCIA:5
-};
-var TYPE_ICONS = [
-	"images/1.png",
-	"images/2.png",
-	"images/3.png",
-	"images/4.png",
-	"images/5.png",
-	"images/6.png"
-];
-var TYPE_LABELS = [
-	"Sem M&eacute;dico",
-	"Demora no Atendimento",
-	"Demora na Ambul&acirc;ncia",
-	"Hospital sem Estrutura",
-	"Falta de Medica&ccedil;&atilde;o",
-	"Neglig&ecirc;ncia"
-];
-
+/*********
+MAP
+**********/
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -30,22 +8,6 @@ function initMap() {
 		zoom: 4,
 		styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
 	});
-}
-
-var data;
-var dataUrl = "data/data.json";
-function loadData(){
-	console.log("data loading: " + dataUrl);
-	$.getJSON(dataUrl, function(json) {
-		console.log("data loaded: " + json);
-		data = json.data;
-		initMarkers();
-	})
-	.fail(function(jqxhr, textStatus, error) {
-		console.log( "loading data error" );
-		var err = textStatus + ", " + error;
-		console.log( "Request Failed: " + err );
-	})
 }
 
 var markers = [];
@@ -63,21 +25,21 @@ function initMarkers(){
 	var markerCluster = new MarkerClusterer(map, markers, {
 		styles: [{
 			url: 'images/group0.png',
-			height: 66,
+			height: 91,
 			width: 66,
 			anchor: [25, 0],
 			textColor: '#ffffff',
 			textSize: 13
 		}, {
 			url: 'images/group1.png',
-			height: 66,
+			height: 91,
 			width: 66,
 			anchor: [25, 0],
 			textColor: '#ffffff',
 			textSize: 13
 		}, {
 			url: 'images/group2.png',
-			height: 66,
+			height: 91,
 			width: 66,
 			anchor: [25, 0],
 			textColor: '#ffffff',
@@ -108,6 +70,81 @@ function showInfo(marker)
 	infoWindow.open(map, marker);
 }
 
+
+/*********
+DATA
+**********/
+var TYPES = {
+	SEM_MEDICO:0,
+	DEMORA_ATENDIMENTO:1,
+	DEMORA_AMBULANCIA:2,
+	HOSPITAL_SEM_ESTRUTURA:3,
+	FALTA_MEDICACAO:4,
+	NEGLIGENCIA:5
+};
+var TYPE_ICONS = [
+	"images/1.png",
+	"images/2.png",
+	"images/3.png",
+	"images/4.png",
+	"images/5.png",
+	"images/6.png"
+];
+var TYPE_LABELS = [
+	"Sem M&eacute;dico",
+	"Demora no Atendimento",
+	"Demora na Ambul&acirc;ncia",
+	"Hospital sem Estrutura",
+	"Falta de Medica&ccedil;&atilde;o",
+	"Neglig&ecirc;ncia"
+];
+
+var data;
+var dataUrl = "data/data.json";
+function loadData(){
+	console.log("data loading: " + dataUrl);
+	$.getJSON(dataUrl, function(json) {
+		console.log("data loaded: " + json);
+		data = json.data;
+		initMarkers();
+	})
+	.fail(function(jqxhr, textStatus, error) {
+		console.log( "loading data error" );
+		var err = textStatus + ", " + error;
+		console.log( "Request Failed: " + err );
+	})
+}
+
+/*********
+UI
+**********/
+var options = [];
+
+$( '.dropdown-menu a' ).on( 'click', function( event ) {
+
+	var $target = $( event.currentTarget ),
+		val = $target.attr( 'data-value' ),
+		$inp = $target.find( 'input' ),
+		idx;
+
+	if ( ( idx = options.indexOf( val ) ) > -1 ) {
+		options.splice( idx, 1 );
+		$inp.prop( 'checked', false );
+	} else {
+		options.push( val );
+		$inp.prop( 'checked', true );
+	}
+
+	$( event.target ).blur();
+
+	console.log( options );
+	return false;
+});
+
+
+/*********
+INIT
+**********/
 function onload() {
 	initMap();
 	loadData();
