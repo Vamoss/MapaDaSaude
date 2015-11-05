fab536e999f8e37302bab256715185e4748b2d03
vamoss <vamoss@vamoss.com.br>
2015-11-05 16:58:14 -0200

    Formulários (Relacionados #3, #7, #8)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: -14.2392976, lng: -53.1805017},
 		zoom: 4,
+		streetViewControl: false,
+		mapTypeControl: false,
 		styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
 	});
 }
@@ -118,34 +120,47 @@ function loadData(){
 /*********
 UI
 **********/
-var options = [];
+var filterOptions = [];
 
-$( '.dropdown-menu a' ).on( 'click', function( event ) {
+function initUI() {
 
-	var $target = $( event.currentTarget ),
-		val = $target.attr( 'data-value' ),
-		$inp = $target.find( 'input' ),
-		idx;
+	//login
+	$('#loginButton').on('click', function( event ) {
+		$('#loginWindow').modal('show');
+	});
 
-	if ( ( idx = options.indexOf( val ) ) > -1 ) {
-		options.splice( idx, 1 );
-		$inp.prop( 'checked', false );
-	} else {
-		options.push( val );
-		$inp.prop( 'checked', true );
-	}
+	//denuncie
+	$('#denuncie').on('click', function( event ) {
+		$('#denuncieWindow').modal('show');
+	});
+	
+	//filters
+	$( '.dropdown-menu a' ).on( 'click', function( event ) {
+		var $target = $( event.currentTarget ),
+			val = $target.attr( 'data-value' ),
+			$inp = $target.find( 'input' ),
+			idx;
 
-	$( event.target ).blur();
+		if ( ( idx = filterOptions.indexOf( val ) ) > -1 ) {
+			filterOptions.splice( idx, 1 );
+			$inp.prop( 'checked', false );
+		} else {
+			filterOptions.push( val );
+			$inp.prop( 'checked', true );
+		}
 
-	console.log( options );
-	return false;
-});
+		$( event.target ).blur();
 
+		console.log( filterOptions );
+		return false;
+	});  
+}
 
 /*********
 INIT
 **********/
 function onload() {
 	initMap();
+	initUI();
 	loadData();
 }
