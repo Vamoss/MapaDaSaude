0ead5330ec80c24be0a7d1595efd643c8e34c62e
Harrison Mendonça <harrison@grama.cc>
2017-01-22 23:57:52 -0200

    add home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,19 @@
 @section('title', 'Mapa da Saúde')
 
 @section('content')
-  <p>Mapa.</p>
+<section id="home">
+  <div class="explore">
+    <h1>Explore o <br>Mapa da Saúde</h1>
+    <a href="{{ url('/mapa') }}">
+      <button>Explore</button>
+    </a>
+  </div>
+  <div class="complain">
+    <h1>Faça agora <br>sua denúncia</h1>
+    <a href="{{ url('/denuncie') }}">
+      <button>Denuncie</button>
+    </a>
+  </div>
+  
+</section>
 @stop
